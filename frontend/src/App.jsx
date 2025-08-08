0702197b199a150b1bd0fb1b0f1933c854603d31
JonBlinder <tearedvpn@gmail.com>
2025-08-08 02:55:39 -0400

    setup react in frontend directory. Need to unit test before moving on.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>MangaRecon</h1>
+      <p>Welcome! Let's start by logging in.</p>
+    </div>
+  );
+}
+
+export default App;
